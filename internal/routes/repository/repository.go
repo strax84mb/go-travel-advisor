7f631ae604f308fdc838740d29a1a89e289af2aa
Strahinja Dobrijevic <strahinjamalabosna@yahoo.com>
2024-05-27 19:58:16 +0200

    Finished routes

--- internal/routes/repository/repository.go
+++ internal/routes/repository/repository.go
@@ -18,24 +18,105 @@ func NewRouteRepository(db *database.Database) *routeRepository {
 	}
 }
 
-func (rr *routeRepository) Find(pagination utils.Pagination) ([]database.Route, error) {
+func (rr *routeRepository) doFind(
+	pagination utils.Pagination,
+	adjustQuery func(*gorm.DB) *gorm.DB,
+) ([]database.Route, error) {
 	var list []database.Route
-	tx := rr.db.DB.Limit(pagination.Limit).Offset(pagination.Offset).Find(&list)
+	tx := rr.db.DB.Limit(pagination.Limit).Offset(pagination.Offset)
+	if adjustQuery != nil {
+		tx = adjustQuery(tx)
+	}
+	tx = tx.Find(&list)
 	if tx.Error != nil {
 		return nil, fmt.Errorf("failed to read routes: %w", tx.Error)
 	}
 	return list, nil
 }
 
-func (rr *routeRepository) listRoutes(
+func (rr *routeRepository) Find(pagination utils.Pagination) ([]database.Route, error) {
+	return rr.doFind(pagination, nil)
+}
+
+func (rr *routeRepository) FindRoutesForAirport(
+	airportID int64,
 	incomming, outgoing bool,
 	pagination utils.Pagination,
-	where func(*gorm.DB) *gorm.DB,
 ) ([]database.Route, error) {
-	var list []database.Route
-	tx := where(rr.db.DB)
-	if incomming {
-		tx = tx.Where("source_id = ?")
-	}
+	return rr.doFind(
+		pagination,
+		func(tx *gorm.DB) *gorm.DB {
+			switch {
+			case incomming && outgoing:
+				tx = tx.Where(tx.Where("routes.destination_id = ?", airportID).Or("routes.source_id = ?", airportID))
+			case incomming && !outgoing:
+				tx = tx.Where("routes.destination_id = ?", airportID)
+			case !incomming && outgoing:
+				tx = tx.Where("routes.source_id = ?", airportID)
+			}
+			return tx
+		},
+	)
+}
 
+func (rr *routeRepository) FindRoutesForCity(
+	cityID int64,
+	incomming, outgoing bool,
+	pagination utils.Pagination,
+) ([]database.Route, error) {
+	return rr.doFind(
+		pagination,
+		func(tx *gorm.DB) *gorm.DB {
+			switch {
+			case incomming && outgoing:
+				tx = tx.Joins("JOIN airports ON airports.id = routes.destination_id OR airports.id = routes.source_id")
+			case incomming && !outgoing:
+				tx = tx.Joins("JOIN airports ON airports.id = routes.destination_id")
+			case !incomming && outgoing:
+				tx = tx.Joins("JOIN airports ON airports.id = routes.source_id")
+			}
+			return tx.Where("airports.city_id = ?", cityID)
+		},
+	)
+}
+
+func (rr *routeRepository) FindByID(id int64, loadAirports bool) (*database.Route, error) {
+	tx := rr.db.DB.Where("id = ?", id)
+	if loadAirports {
+		tx = tx.Preload("Source").Preload("Destination")
+	}
+	var route database.Route
+	tx = tx.Take(&route)
+	switch {
+	case tx.Error == gorm.ErrRecordNotFound:
+		return nil, database.ErrNotFound
+	case tx.Error != nil:
+		return nil, fmt.Errorf("failed to read route: %w", tx.Error)
+	default:
+		return &route, nil
+	}
+}
+
+func (rr *routeRepository) Insert(route database.Route) (*database.Route, error) {
+	tx := rr.db.DB.Create(&route)
+	if tx.Error != nil {
+		return nil, fmt.Errorf("failed to save new route: %w", tx.Error)
+	}
+	return &route, nil
+}
+
+func (rr *routeRepository) UpdatePrice(id int64, price float32) error {
+	tx := rr.db.DB.Where("id = ?", id).Update("price", price)
+	if tx.Error != nil {
+		return fmt.Errorf("failed to update route price: %w", tx.Error)
+	}
+	return nil
+}
+
+func (rr *routeRepository) Delete(id int64) error {
+	tx := rr.db.DB.Where("id = ?", id).Delete(&database.Route{})
+	if tx.Error != nil {
+		return fmt.Errorf("failed to delete route: %w", tx.Error)
+	}
+	return nil
 }
--- internal/routes/route_service.go
+++ internal/routes/route_service.go
@@ -11,6 +11,20 @@ import (
 
 type iRouteRepository interface {
 	Find(pagination utils.Pagination) ([]database.Route, error)
+	FindRoutesForAirport(
+		airportID int64,
+		incomming, outgoing bool,
+		pagination utils.Pagination,
+	) ([]database.Route, error)
+	FindRoutesForCity(
+		cityID int64,
+		incomming, outgoing bool,
+		pagination utils.Pagination,
+	) ([]database.Route, error)
+	FindByID(id int64, loadAirports bool) (*database.Route, error)
+	Insert(route database.Route) (*database.Route, error)
+	UpdatePrice(id int64, price float32) error
+	Delete(id int64) error
 }
 
 type iAirportRepository interface{}
@@ -44,26 +58,101 @@ func (rs *routeService) ListRoutes(ctx context.Context, pagination utils.Paginat
 	return list, nil
 }
 
-func (rs *routeService) ListRoutesForCity(ctx context.Context, cityID int64, direction Direction, pagination utils.Pagination) ([]database.Route, error) {
-	return nil, nil
+// Returns booleans from Direction
+// Format is (incomming bool, outgoing bool)
+func boolsFromDirection(direction Direction) (bool, bool) {
+	switch direction {
+	case DIRECTION_INCOMMING:
+		return true, false
+	case DIRECTION_OUTGOING:
+		return false, true
+	default:
+		return true, true
+	}
 }
 
-func (rs *routeService) ListRoutesForAirport(ctx context.Context, airportID int64, direction Direction, pagination utils.Pagination) ([]database.Route, error) {
-	return nil, nil
+func (rs *routeService) ListRoutesForCity(
+	ctx context.Context,
+	cityID int64,
+	direction Direction,
+	pagination utils.Pagination,
+) ([]database.Route, error) {
+	incomming, outgoing := boolsFromDirection(direction)
+	list, err := rs.routeRepo.FindRoutesForCity(cityID, incomming, outgoing, pagination)
+	if err != nil {
+		logrus.WithContext(ctx).WithError(err).
+			WithFields(logrus.Fields{
+				"cityId":              cityID,
+				"listIncommingRoutes": incomming,
+				"listOutgoingRoutes":  outgoing,
+			}).
+			Error("could not list routes for city")
+		return nil, fmt.Errorf("could not list routes for city: %w", err)
+	}
+	return list, nil
+}
+
+func (rs *routeService) ListRoutesForAirport(
+	ctx context.Context,
+	airportID int64,
+	direction Direction,
+	pagination utils.Pagination,
+) ([]database.Route, error) {
+	incomming, outgoing := boolsFromDirection(direction)
+	list, err := rs.routeRepo.FindRoutesForAirport(airportID, incomming, outgoing, pagination)
+	if err != nil {
+		logrus.WithContext(ctx).WithError(err).
+			WithFields(logrus.Fields{
+				"airportId":           airportID,
+				"listIncommingRoutes": incomming,
+				"listOutgoingRoutes":  outgoing,
+			}).
+			Error("could not list routes for airport")
+		return nil, fmt.Errorf("could not list routes for airport: %w", err)
+	}
+	return list, nil
 }
 
 func (rs *routeService) RouteByID(ctx context.Context, id int64) (*database.Route, error) {
-	return nil, nil
+	route, err := rs.routeRepo.FindByID(id, true)
+	if err != nil {
+		logrus.WithContext(ctx).WithError(err).
+			WithField("routeId", id).
+			Error("failed to read route")
+		return nil, fmt.Errorf("failed to read route: %w", err)
+	}
+	return route, nil
 }
 
 func (rs *routeService) SaveNew(ctx context.Context, route database.Route) (*database.Route, error) {
-	return nil, nil
+	savedRoute, err := rs.routeRepo.Insert(route)
+	if err != nil {
+		logrus.WithContext(ctx).WithError(err).
+			Error("could not save route: %w", err)
+	}
+	return savedRoute, nil
 }
 
 func (rs *routeService) UpdateRoutePrice(ctx context.Context, id int64, price float32) error {
+	if err := rs.routeRepo.UpdatePrice(id, price); err != nil {
+		logrus.WithContext(ctx).WithError(err).
+			WithFields(logrus.Fields{
+				"routeId":  id,
+				"newPrice": price,
+			}).
+			Error("could not update route price")
+		return fmt.Errorf("could not update route price: %w", err)
+	}
 	return nil
 }
 
 func (rs *routeService) DeleteRoute(ctx context.Context, id int64) error {
+	err := rs.routeRepo.Delete(id)
+	if err != nil {
+		logrus.WithContext(ctx).WithError(err).
+			WithField("routeId", id).
+			Error("could not delete route")
+		return fmt.Errorf("could not delete route: %w", err)
+	}
 	return nil
 }
